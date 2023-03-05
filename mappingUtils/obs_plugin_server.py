ca1615c5fedfb3f0710e4d9f0769023f47241f09
luna <lunameadows@protonmail.com>
2023-03-04 23:24:28 -0600

    Linux version of the OBS Call Mapper completed in it's basic stage. Some testing is still needed and is not offically supported on Windows OR Mac.

---
+++ mappingUtils/obs_plugin_server.py
@@ -0,0 +1,38 @@
+"""
+Server side of the obs plugin. Used to send information to obs
+"""
+import socket
+
+class Server:
+    """
+        OBSPluginServer is the server host for the obs plugin to map cameras inside obs.
+        It connects to obs and then sends commands to the plugin with camera positions, names, and other info needed.
+
+        Attributes
+        ----------
+        host : int
+            host ip address
+        port : int
+            port for server communication
+        """
+    def __init__(self, port=48387):
+        """
+        Sets up global variables for server
+
+        :param port: port for sever communication
+        """
+        self.port = port
+        self.server_socket = None
+
+    def start_server(self):
+        """
+        Starts server for the obs plugin
+        """
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send_command(self, msg):
+        """
+        Sends command to obs plugin client
+        :param msg: command message
+        """
+        self.server_socket.sendto(bytes(str(msg), "utf-8"), ('', self.port))
